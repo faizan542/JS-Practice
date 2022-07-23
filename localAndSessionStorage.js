62f096bedbcbacbb660242217065e82f598cb671
faizan542 <faizansilat100@gmail.com>
2022-07-23 18:02:56 +0500

    local and session storage practice

--- localAndSessionStorage.js
+++ localAndSessionStorage.js
@@ -1,14 +1,15 @@
-// Add a key value in local storage
+//// LOCAL STORAGE
+//// Add a key value in local storage
 localStorage.setItem('Name', 'Faizan');
 localStorage.setItem('Name2', 'Ghulam Qadir');
 
-// Clear an entire local storage 
+//// Clear an entire local storage
 // localStorage.clear();
 
-// remove a particular key value form local storage
-localStorage.removeItem('Name2')
+//// remove a particular key value form local storage
+// localStorage.removeItem('Name2')
 
-// Get Item from the local storage
+//// Get Item from the local storage
 let name = localStorage.getItem('Name');
 console.log(name);
 
@@ -17,3 +18,24 @@ localStorage.setItem('Cities', JSON.stringify(arrr));
 
 let cityName = JSON.parse(localStorage.getItem('Cities'));
 console.log(cityName)
+
+//// SESSION STORAGE
+//// Add a key value in session storage
+sessionStorage.setItem('sessionName', 'Faizan');
+sessionStorage.setItem('sessionName2', 'Ghulam Qadir');
+
+//// Clear an entire session storage
+// sessionStorage.clear();
+
+//// remove a particular key value form session storage
+// sessionStorage.removeItem('sessionName2')
+
+//// Get Item from the session storage
+let sessionName = sessionStorage.getItem('sessionName2');
+console.log(sessionName);
+
+let countryArray = ['Pakistan', 'Turkey', 'Saudia Arabia'];
+sessionStorage.setItem('Country', JSON.stringify(arrr));
+
+let getCountryArray = JSON.parse(sessionStorage.getItem('Country'));
+console.log(countryArray);
