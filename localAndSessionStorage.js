8e8a013df9baabf97433c607cdb1f651e3bf085b
faizan542 <faizansilat100@gmail.com>
2022-07-23 17:45:20 +0500

    local and session storage practice

--- localAndSessionStorage.js
+++ localAndSessionStorage.js
@@ -1,5 +1,19 @@
+// Add a key value in local storage
 localStorage.setItem('Name', 'Faizan');
 localStorage.setItem('Name2', 'Ghulam Qadir');
+
+// Clear an entire local storage 
+// localStorage.clear();
+
+// remove a particular key value form local storage
+localStorage.removeItem('Name2')
+
+// Get Item from the local storage
 let name = localStorage.getItem('Name');
 console.log(name);
 
+let arrr = ['Karachi', 'Lahore', 'Islamabad'];
+localStorage.setItem('Cities', JSON.stringify(arrr));
+
+let cityName = JSON.parse(localStorage.getItem('Cities'));
+console.log(cityName)
